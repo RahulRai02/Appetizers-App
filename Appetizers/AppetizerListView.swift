01025984eb65df0170840076f276d779bfe876ae
Rahul Rai <rahul@Rahuls-MacBook-Air.local>
2023-11-07 00:54:21 +0530

    View Model Refactoring

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,37 +8,18 @@
 import SwiftUI
 
 struct AppetizerListView: View {
-    @State private var appetizers: [Appetizer] = []
+    
+    @StateObject var viewModel = AppetizerListViewModel()
     
     var body: some View {
         NavigationView{
-            List(appetizers, id: \.id){ appetizer in
+            List(viewModel.appetizers, id: \.id){ appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍔Appetizers")
         }
         .onAppear {
-            getAppetizers()
-        }
-    }
-    
-    // API Call
-    func getAppetizers(){
-        NetworkManager.shared.getAppetizers { result in
-            // Now result is either gonna be a success or a failure
-            // Now the network call is happening on a background thread.
-            // But on SwiftUI, we need to make network call on the main thread....
-            // [REMEMBER] this.....
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-            
-
+            viewModel.getAppetizers()
         }
     }
     
